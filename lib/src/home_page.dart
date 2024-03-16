a229a3f084e9951fdd42070bfcb20653c5bbcdb8
RusdyZain <ryanfajri28@gmail.com>
2024-03-16 14:29:47 +0800

    UI Done

---
+++ lib/src/home_page.dart
@@ -0,0 +1,130 @@
+import 'package:chatbot_flutter/widget/feature_box.dart';
+import 'package:chatbot_flutter/widget/pallete.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friday'),
+        leading: Icon(
+          Icons.menu,
+        ),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          // Profile Virtual Assisten
+          Stack(
+            children: [
+              Center(
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  margin: const EdgeInsets.only(top: 4),
+                  decoration: const BoxDecoration(
+                    color: Pallete.assistantCircleColor,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Container(
+                height: 123,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(
+                      "assets/images/virtualAssistant.png",
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // Bubble Chat
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            margin:
+                const EdgeInsets.symmetric(horizontal: 40).copyWith(top: 30),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Pallete.borderColor,
+              ),
+              borderRadius:
+                  BorderRadius.circular(20).copyWith(topLeft: Radius.zero),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: const Text(
+                "Good Morning, what task can i do for you?",
+                style: TextStyle(
+                  color: Pallete.mainFontColor,
+                  fontSize: 15,
+                  fontFamily: "Cera Pro",
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(10),
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(
+              top: 10,
+              left: 22,
+            ),
+            child: const Text(
+              "Here are a few features",
+              style: TextStyle(
+                fontFamily: "Cera Pro",
+                color: Pallete.mainFontColor,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          // Features List Chat
+          Column(
+            children: [
+              FeatureBox(
+                color: Pallete.firstSuggestionBoxColor,
+                headerText: "ChatGPT",
+                descriptionText:
+                    "A smarter way to stay orgenaized and informed with ChatGPT",
+              ),
+              FeatureBox(
+                color: Pallete.secondSuggestionBoxColor,
+                headerText: 'Dall-E',
+                descriptionText:
+                    'Get inspired and stay creative with your personal assistant powered by Dall-E',
+              ),
+              FeatureBox(
+                color: Pallete.thirdSuggestionBoxColor,
+                headerText: 'Smart Voice Assistant',
+                descriptionText:
+                    'Get the best of both worlds with a voice assistant powered by Dall-E and ChatGPT',
+              ),
+            ],
+          ),
+          FloatingActionButton(
+            backgroundColor: Pallete.firstSuggestionBoxColor,
+            onPressed: () {},
+            child: Icon(
+              Icons.mic,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
